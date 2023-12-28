55a145447d2007fadd09008db0dfcd6d82e829fd
tomo <09bkhair19@gmail.com>
2023-12-28 10:50:06 +0900

    made retry

--- public/game.js
+++ public/game.js
@@ -1,5 +1,5 @@
 import {Cell,Mino,Field,Coordinate} from "./logic.js";
-let time=60;
+let time=10;
 function timer_start(){
     function timer_update(){
         time--;
@@ -7,9 +7,14 @@ function timer_start(){
         if(time==0){
             clearInterval(timer);
             console.log("timeup");
+            retry();
             //GameEnd();
         }
     }
     let timer=setInterval(timer_update,1000);
 }
 timer_start();
+
+function retry(){
+    location.reload();
+}
