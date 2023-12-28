d72653d6b145cb8e3a3ba34025d8ab1ad34016ee
aisuQwQ <aisuQwQ@ya.ru>
2023-12-28 13:58:32 +0900

    fix: add id

--- public/mino.js
+++ public/mino.js
@@ -4,54 +4,73 @@ const emptyCell = new Cell(-1, -1);
 
 class IMino extends Mino {
   constructor(parentId) {
-    super([
+    super(
       [
-        new Cell(parentId, -2),
-        new Cell(parentId, -2),
-        new Cell(parentId, -2),
-        new Cell(parentId, -2),
+        [
+          new Cell(parentId, -2),
+          new Cell(parentId, -2),
+          new Cell(parentId, -2),
+          new Cell(parentId, -2),
+        ],
       ],
-    ]);
+      1
+    );
   }
 }
 
 class OMino extends Mino {
   constructor(parentId) {
-    super([
-      [new Cell(parentId, -2), new Cell(parentId, -2)],
-      [new Cell(parentId, -2), new Cell(parentId, -2)],
-    ]);
+    super(
+      [
+        [new Cell(parentId, -2), new Cell(parentId, -2)],
+        [new Cell(parentId, -2), new Cell(parentId, -2)],
+      ],
+      2
+    );
   }
 }
 
 class LMino extends Mino {
   constructor(parentId) {
     const o = emptyCell;
-    super([
-      [new Cell(parentId, -2), o],
-      [new Cell(parentId, -2), o],
-      [new Cell(parentId, -2), new Cell(parentId, -2)],
-    ]);
+    super(
+      [
+        [new Cell(parentId, -2), o],
+        [new Cell(parentId, -2), o],
+        [new Cell(parentId, -2), new Cell(parentId, -2)],
+      ],
+      3
+    );
   }
 }
 class SMino extends Mino {
   constructor(parentId) {
     const o = emptyCell;
-    super([
-      [new Cell(parentId, -2), o],
-      [new Cell(parentId, -2), new Cell(parentId, -2)],
-      [o, new Cell(parentId, -2)],
-    ]);
+    super(
+      [
+        [new Cell(parentId, -2), o],
+        [new Cell(parentId, -2), new Cell(parentId, -2)],
+        [o, new Cell(parentId, -2)],
+      ],
+      4
+    );
   }
 }
 
 class TMino extends Mino {
   constructor(parentId) {
     const o = emptyCell;
-    super([
-      [new Cell(parentId, -2), new Cell(parentId, -2), new Cell(parentId, -2)],
-      [o, new Cell(parentId, -2), o],
-    ]);
+    super(
+      [
+        [
+          new Cell(parentId, -2),
+          new Cell(parentId, -2),
+          new Cell(parentId, -2),
+        ],
+        [o, new Cell(parentId, -2), o],
+      ],
+      5
+    );
   }
 }
 
