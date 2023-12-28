804cfea60f5106427da43706fe2611627d7f8f90
aisuQwQ <aisuQwQ@ya.ru>
2023-12-28 10:42:59 +0900

    feat: hide title

--- public/modal.js
+++ public/modal.js
@@ -1,1 +1,14 @@
-export function showTitle() {}
+//モーダルのボタンにイベントリスナーつける
+export function initModal() {
+    //title
+    document
+        .querySelector("#modal-title button")
+        .addEventListener("click", hideTitle);
+}
+
+function hideTitle() {
+    const blackDom = document.getElementById("modal-black");
+    blackDom.classList.add("hide");
+    const titleDom = document.getElementById("modal-title");
+    titleDom.classList.add("hide");
+}
