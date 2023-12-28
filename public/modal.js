ed4c0910e4911df5bb2d248210b0ec184f0e27bf
aisuQwQ <aisuQwQ@ya.ru>
2023-12-28 15:17:23 +0900

    feat: help

--- public/modal.js
+++ public/modal.js
@@ -11,6 +11,12 @@ export function initModal() {
     .addEventListener("click", () => {
       location.reload();
     });
+  //help
+  initHelp();
+  document.getElementById("help").addEventListener("click", showHelp);
+  document
+    .querySelector("#modal-help button")
+    .addEventListener("click", hideHelp);
 }
 
 function hideTitle() {
@@ -27,6 +33,49 @@ export function showRetry() {
   blackDom.classList.remove("hide");
   const titleDom = document.getElementById("modal-retry");
   titleDom.classList.remove("hide");
+}
 
-  
+function initHelp() {
+  const type = [
+    "えび",
+    "かまぼこ",
+    "ごぼう",
+    "なます",
+    "伊達巻",
+    "錦玉子",
+    "金柑",
+    "栗きんとん",
+    "黒豆",
+    "昆布巻き",
+    "酢だこ",
+    "数の子",
+    "田作り",
+    "八幡巻き",
+    "蓮根",
+  ];
+  const body = document.querySelector("#modal-help .modal-body");
+  type.forEach((type) => {
+    const row = document.createElement("div");
+    row.classList.add("help-row");
+    const img = document.createElement("img");
+    img.src = "./assets/" + type + ".png";
+    img.style.marginRight = "5px";
+    const name = document.createElement("div");
+    name.innerHTML = type;
+    row.appendChild(img);
+    row.appendChild(name);
+    body.appendChild(row);
+  });
+}
+function showHelp() {
+  const blackDom = document.getElementById("modal-black");
+  blackDom.classList.remove("hide");
+  const titleDom = document.getElementById("modal-help");
+  titleDom.classList.remove("hide");
+}
+function hideHelp() {
+  const blackDom = document.getElementById("modal-black");
+  blackDom.classList.add("hide");
+  const titleDom = document.getElementById("modal-help");
+  titleDom.classList.add("hide");
 }
